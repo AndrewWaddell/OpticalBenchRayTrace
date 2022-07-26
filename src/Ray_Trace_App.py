c4acd0cda3c1c55b195c438d8365da312aae13e9
AndoWadso <raytracesoftware@gmail.com>
2022-07-26 17:47:05 +1000

    Python saves results to text file

--- src/Ray_Trace_App.py
+++ src/Ray_Trace_App.py
@@ -6,6 +6,7 @@ Created on Fri Feb 25 16:29:15 2022
 """
 
 import numpy as np
+import csv
 
 
 class Scene:
@@ -366,5 +367,18 @@ test_bench.add_source()
 test_bench.add_shape()
 test_bench.trace()
 
+# save results to text file
 
+with open('data_transfer.txt','w') as f:
+    f.truncate()
+    f.write('pacc\n')
+    f.write(str(test_bench.rays.pacc)+'\n')
+    f.write('upacc\n')
+    f.write(str(test_bench.rays.upacc)+'\n')
+    f.write('dacc\n')
+    f.write(str(test_bench.rays.dacc)+'\n')
+    f.write('cm\n')
+    f.write(str(test_bench.shapes[0].cm)+'\n')
+    f.write('sp\n')
+    f.write(str(test_bench.shapes[0].p))
 
